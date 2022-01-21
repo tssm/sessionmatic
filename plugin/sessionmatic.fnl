3d3ab1b09bf1bfba457069af1e639f871bf7aa2f
Tae Selene Sandoval Murgan <tssm@users.noreply.github.com>
2022-01-21 21:48:31 +0100

    Rewrite in Fennel

---
+++ plugin/sessionmatic.fnl
@@ -0,0 +1,28 @@
+(local cmd vim.cmd)
+(local f vim.fn)
+
+(global Sessionmatic {})
+
+(fn load []
+  (local session-file :Session.vim)
+  (when
+    (and
+      (= (f.argc) 0)
+      (= (f.filereadable session-file) 1))
+    (cmd (string.format "source %s" session-file))))
+(set Sessionmatic.load load)
+
+(fn save []
+  (local this-session vim.v.this_session)
+  (when
+    (and
+      (not= this-session null)
+      (= (f.filewritable this-session) 1))
+    (cmd :mksession!)))
+(set Sessionmatic.save save)
+
+(cmd "augroup Sessionmatic")
+(cmd :autocmd!)
+(cmd "autocmd DirChanged,VimEnter * nested lua Sessionmatic.load()")
+(cmd "autocmd VimLeavePre * lua Sessionmatic.save()")
+(cmd "augroup END")
---
+++ plugin/sessionmatic.lua
@@ -0,0 +1,27 @@
+local _2afile_2a = "plugin/sessionmatic.fnl"
+local cmd = vim.cmd
+local f = vim.fn
+Sessionmatic = {}
+local function load()
+  local session_file = "Session.vim"
+  if ((f.argc() == 0) and (f.filereadable(session_file) == 1)) then
+    return cmd(string.format("source %s", session_file))
+  else
+    return nil
+  end
+end
+Sessionmatic.load = load
+local function save()
+  local this_session = vim.v.this_session
+  if ((this_session ~= null) and (f.filewritable(this_session) == 1)) then
+    return cmd("mksession!")
+  else
+    return nil
+  end
+end
+Sessionmatic.save = save
+cmd("augroup Sessionmatic")
+cmd("autocmd!")
+cmd("autocmd DirChanged,VimEnter * nested lua Sessionmatic.load()")
+cmd("autocmd VimLeavePre * lua Sessionmatic.save()")
+return cmd("augroup END")
